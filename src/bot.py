e0b38e49f03d5d27f9f8c227565f1ebaddf77998
Desasth <invicems@gmail.com>
2021-03-31 22:12:45 +0400

    added bot.py, parser modified

---
+++ src/bot.py
@@ -0,0 +1,42 @@
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import logging
+from multiprocessing.pool import ThreadPool
+import hound_parser
+
+logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+        level = logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def start(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text("Hello!")
+    update.message.reply_text("Use /get_cost to get cost")
+    update.message.reply_text("Use /get_income to get income")
+
+def get_cost(update: Update, _: CallbackContext) -> None:
+    pool = ThreadPool(processes=1)
+    cost = pool.apply_async(hound_parser.parse_cost)
+    cost = cost.get()
+    update.message.reply_text("Стоимость портфеля: " + cost)
+
+def get_income(update: Update, _: CallbackContext) -> None:
+    pool = ThreadPool(processes=1)
+    income = pool.apply_async(hound_parser.parse_income)
+    income = income.get()
+    update.message.reply_text("Прибыль: " + income)
+
+def main():
+    updater = Updater(token = 'put your token here', use_context = True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("get_cost", get_cost))
+    dispatcher.add_handler(CommandHandler("get_income", get_income))
+    
+    updater.start_polling()
+    updater.idle()
+    
+
+if __name__ == '__main__':
+    main()
--- src/hound_parser.py
+++ src/hound_parser.py
@@ -3,26 +3,23 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-def main():
-    link = 'https://intelinvest.ru/public-portfolio/id' # instead of this link put your own public-portfolio link
+def get_page():
+    link = 'https://intelinvest.ru/public-portfolio/id/' # put your public-portfolio link here
     
-    page = get_page(link)
-    parse_data(page)
-
-def get_page(link):
     return (requests.get(link))
 
-def parse_data(page):
+def parse_cost():
+    page = get_page()
     data = bs(page.text, 'html.parser')
-    
     cost = data.find('span', class_= 'dashboard-currency dashboard-card-big-nums rub')
+
+    return cost.text
+
+def parse_income():
+    page = get_page()
+    data = bs(page.text, 'html.parser')
 
     income = data.find('div', class_= 'flex dashboard-item xl3 lg3 md6 sm6 xs12 align-content-start')
     income = income.find('span', class_= 'dashboard-currency dashboard-card-big-nums rub')
 
-    print(cost.text)
-    print(income.text)
-    
-
-if __name__ == '__main__':
-    main()
+    return income.text
