52c42edea933535a4345deabf9450f7d387a175a
Desasth <invicems@gmail.com>
2021-04-12 00:44:51 +0400

    updated hound_bot.py

--- src/microservices/hound_bot/hound_bot.py
+++ src/microservices/hound_bot/hound_bot.py
@@ -1,10 +1,9 @@
 # @Author: Daniil Maslov (ComicSphinx)
 
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
-import requests, json, logging, time
-from telegram import Update, Message
+import requests, json, logging, time, schedule
+from telegram import Update, Bot
 from telegram.ext import Updater, CommandHandler, CallbackContext
+from multiprocessing.pool import ThreadPool
 
 logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
         level = logging.INFO)
@@ -12,7 +11,8 @@ logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message
 logger = logging.getLogger(__name__)
 
 def handler_start(update, context):
-    print("/start")
+    print(update.effective_chat.id, ":", "was connected and used /start")
+
     update.message.reply_text("Hello!")
     update.message.reply_text("Use /get_cost to get cost")
     update.message.reply_text("Use /get_income to get income")
@@ -23,31 +23,42 @@ def get_data():
     data = data.json()
     return data
 
-def handler_get_cost(update: Update, _: CallbackContext) -> None:
-    print("/get_cost")
+def handler_get_cost(update: Update, context: CallbackContext) -> None:
+    print(update.effective_chat.id, ":", "/get_cost")
     data = get_data()
     update.message.reply_text("Investment portfolio value: " + str(data['cost']) + "Rub")
 
-def handler_get_income(update: Update, _: CallbackContext) -> None:
-    print("/get_income")
+def handler_get_income(update: Update, context: CallbackContext) -> None:
+    print(update.effective_chat.id, ":", "/get_income")
     data = get_data()
     update.message.reply_text("Investment portfolio income: " + str(data['income']) + "Rub")
 
-def handler_get_auto_info(update: Update, _: CallbackContext) -> None:
-    print("/get_auto_info")
-    goal_income = 300
-    # wait 60 minutes
-    time_wait = 60 * 60
-    # wait 5 seconds (only for test the app)
-    # time_wait = 5
+def handler_get_auto_info(update: Update, context: CallbackContext) -> None:
+    print(update.effective_chat.id, ":", "/get_auto_info")
+    # TODO: В какой-то момент этот поток нужно убивать
+    pool = ThreadPool(processes=1)
+    get_parse_data = pool.apply_async(income_scheduler, args=(context, update))
 
+def income_scheduler(context, update):
+    print(update.effective_chat.id, ":", "launched income scheduler")
+    # do it every hour
+    schedule.every(60).minutes.do(auto_update_info, context=context, update=update)
+    
     while(True):
-        time.sleep(time_wait)
-        data = get_data()
+        schedule.run_pending()
 
-        print("float(data['income'])", float(data['income']))
-        if (goal_income <= float(data['income'])):
-            update.message.reply_text("Congratulations! Goal achieved! Your income:" + str(data['income']) + "Rub")
+# TODO: need to rename
+def auto_update_info(context, update):
+    print(update.effective_chat.id, ":", "auto update income info")
+    goal_income = 300
+    data = get_data()
+
+    if (goal_income <= float(data['income'])):
+        arg_str = "Congratulations! Goal achieved! Your income:" + str(data['income']) + "Rub"
+        send_message(context, update.effective_chat.id, arg_str)
+
+def send_message(context, chat_id, str):
+    context.bot.sendMessage(chat_id, str)
 
 # def handler_set_goal(update: Update, context: CallbackContext) -> None:
 #         try:
