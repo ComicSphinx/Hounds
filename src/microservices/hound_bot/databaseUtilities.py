fd5493ac7f43554cf6602293977ea1229cd81d0e
ComicSphinx <maslov2daniil@gmail.com>
2021-04-17 01:08:39 +0400

    integration with database

--- src/microservices/hound_bot/databaseUtilities.py
+++ src/microservices/hound_bot/databaseUtilities.py
@@ -5,31 +5,74 @@ import os
 import sys
 
 database_file_path = "database.db"
-tableName = " users_data "
+tableName = " users "
 
-def verifyDatabaseExist(self):
-    if (os.path.exists(self.database_file_path)):
+def verifyDatabaseExist():
+    if (os.path.exists(database_file_path)):
         return 1
     else:
         return 0
 
-def createDB(self):
-    connection, cursor = self.connectDB(self)
-    cursor.execute("CREATE TABLE"+self.tableName+"(tg_uid int, parse_link VARCHAR(50), goal_profit int, auto_update_time int);")
-    self.saveAndCloseDB(self, connection)
+def createDB():
+    connection, cursor = connectDB()
+    request = "CREATE TABLE"+tableName+"(chat_uid int, portfolio_url VARCHAR(50), goal_profit int);"
+    executeRequest(request)
+    output = saveAndCloseDB(connection)
+    print("db created")
 
-def connectDB(self):
-    connection = sqlite3.connect(self.database_file_path)
+def connectDB():
+    connection = sqlite3.connect(database_file_path)
     cursor = connection.cursor()
     return connection, cursor
 
-def saveAndCloseDB(self, connection):
+def saveAndCloseDB(connection):
     connection.commit()
     connection.close()
 
-def executeCommand(self, insert):
-    connection, cursor = self.connectDB(self)
-    cursor.execute(insert)
+def executeRequest(request):
+    connection, cursor = connectDB()
+    cursor.execute(request)
     output = cursor.fetchall()
-    self.saveAndCloseDB(self,connection)
+    saveAndCloseDB(connection)
     return output
+
+def addData(chat_uid, column, data):
+    print(chat_uid, "attempts to add data")
+    request = "UPDATE" + tableName + "SET " + str(column) + " = '" + str(data) + "' WHERE chat_uid = " + str(chat) 
+    if (verifyDBContainsChat(chat_uid) == 1):
+        print(chat_uid, "Using request :", request)
+        executeRequest(request)
+        print(chat_uid, "Data successfully added")
+    elif (verifyDBContainsChat(chat_uid) == 0):
+        print(chat_uid, "user not found, user will be added")
+        addUser(chat_uid)
+        print(chat_uid, "user added to db")
+        executeRequest(request)
+        print(chat_uid, "data successfully added")
+    else:
+        print("Unrecognized error")
+
+def addUser(chat_uid):
+    if (verifyDBContainsChat(chat_uid) == 0):
+        request = "INSERT INTO" + tableName + "(" + "chat_uid" + ")" + "VALUES (" + str(chat_uid) + ");"
+        executeRequest(request)
+        print(chat_uid, "user successfully added")
+
+def verifyDBContainsChat(chat_uid):
+    output = get_user(chat_uid)
+    if (output == []):
+        print("DB does not contains chat :", chat_uid)
+        return 0
+    else:
+        print("DB contains chat :", chat_uid)
+        return 1
+
+def get_user(chat_uid):
+    request = "SELECT * FROM"+tableName+"WHERE chat_uid ="+str(chat_uid)
+    connection, cursor = connectDB()
+    output = executeRequest(request)
+    return output
+
+def test():
+    print("test")
+    return "test"
--- src/microservices/hound_bot/hound_bot.py
+++ src/microservices/hound_bot/hound_bot.py
@@ -3,6 +3,7 @@
 import requests, json, logging, time, schedule
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from multiprocessing.pool import ThreadPool
+import databaseUtilities as dbu
 
 logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
         level = logging.INFO)
@@ -12,26 +13,31 @@ logger = logging.getLogger(__name__)
 flag_goal_achieved = False
 
 def handler_start(update, context):
-    print(update.effective_chat.id, ":", "was connected and used /start")
+    print(update.effective_chat.id, ":", "used /start or /help")
 
-    update.message.reply_text("Hello!")
+    dbu.addUser(update.effective_chat.id)
+
+    update.message.reply_text("Use /help to get help with commands")
     update.message.reply_text("Use /get_cost to get cost")
     update.message.reply_text("Use /get_income to get income")
     update.message.reply_text("Use /set_goal <value> to set goal")
+    update.message.reply_text("Use /set_page_url <url> to set portfolio url")
 
-def get_data():
-    data = requests.get("http://127.0.0.1:5000/get/id")
+def get_data(chat_uid):
+    user_data = dbu.get_user(chat_uid)
+    url = user_data[0][1]
+    data = requests.get("http://127.0.0.1:5000/get/"+str(url))
     data = data.json()
     return data
 
 def handler_get_cost(update, context):
     print(update.effective_chat.id, ":", "/get_cost")
-    data = get_data()
+    data = get_data(update.effective_chat.id)
     update.message.reply_text("Investment portfolio value: " + str(data['cost']) + "Rub")
 
 def handler_get_income(update, context):
     print(update.effective_chat.id, ":", "/get_income")
-    data = get_data()
+    data = get_data(update.effective_chat.id)
     update.message.reply_text("Investment portfolio income: " + str(data['income']) + "Rub")
 
 def handler_set_goal(update, context):
@@ -70,33 +76,36 @@ def track_goal(context, update, goal_value):
         arg_str = "Congratulations! Goal achieved! Your income:" + str(data['income']) + "Rub"
         context.bot.sendMessage(update.effective_chat.id, arg_str)
 
-# def handler_set_goal(update: Update, context: CallbackContext) -> None:
-#         try:
-#             # args[0] should contain the time for the timer in seconds
-#             due = int(context.args[0])
-#             if due < 0:
-#                 update.message.reply_text('Sorry we can not go back to future!')
-#                 return
+def handler_set_page_url(update, context):
+    print(update.effective_chat.id, ":", "/set_page_url")
 
-#             context.job_queue.run_once(set_goal, due)
+    try:
+        portfolio_url = str(context.args[0])
+        column = "portfolio_url"
+        print(update.effective_chat.id, "trying to add portfolio url = ", portfolio_url)
+        dbu.addData(update.effective_chat.id, column, portfolio_url)
 
-#             text = 'Goal successfully updated'
-#             update.message.reply_text(text)
-
-#         except (IndexError, ValueError):
-#             update.message.reply_text('Usage: /set_goal <value>')
-
-# def set_goal(number):
-#     goal_income = number
+    except (IndexError, ValueError):
+        print(update.effective_chat.id, ": failed attempt to set portfolio url")
+        update.message.reply_text("Usage: /set_page_url <url>")
 
 def main():
+    if (dbu.verifyDatabaseExist() == 0):
+        print("Database is not exists")
+        dbu.createDB()
+        print("Database created")
+    elif (dbu.verifyDatabaseExist() == 1):
+        print("Database found")
+
     updater = Updater(token = 'put your token here', use_context = True)
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", handler_start))
+    dispatcher.add_handler(CommandHandler("help", handler_start))
     dispatcher.add_handler(CommandHandler("get_cost", handler_get_cost))
     dispatcher.add_handler(CommandHandler("get_income", handler_get_income))
     dispatcher.add_handler(CommandHandler("set_goal", handler_set_goal))
+    dispatcher.add_handler(CommandHandler("set_page_url", handler_set_page_url))
 
     updater.start_polling()
     updater.idle()
